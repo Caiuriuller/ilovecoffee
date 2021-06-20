749e2a4ea4841dbb1807e4be86eef14aca1b8c98
Caiuriuller <caiuridh@gmail.com>
2021-06-20 20:59:49 -0300

    feature: added endpoints to add a new coffee

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CoffeesController } from './coffees/coffees.controller';
+import { CoffeeService } from './coffee/coffee.service';
+import { CoffeesService } from './coffees/coffees.service';
+import { CoffeesModule } from './coffees/coffees.module';
+
+@Module({
+  imports: [CoffeesModule],
+  controllers: [AppController, CoffeesController],
+  providers: [AppService, CoffeeService, CoffeesService],
+})
+export class AppModule {}
